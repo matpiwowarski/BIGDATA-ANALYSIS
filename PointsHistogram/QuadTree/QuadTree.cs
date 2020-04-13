a979985c76df4f88a15c02fa2ebf6717591c128e
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2020-04-13 15:15:34 +0200

    Insert QuadTree

--- PointsHistogram/QuadTree/QuadTree.cs
+++ PointsHistogram/QuadTree/QuadTree.cs
@@ -6,7 +6,7 @@ namespace QuadTree
         public Position TopLeft;
         public Position BotRight;
 
-        public Point CurrentNode;
+        public Point CurrentPoint;
 
         // Children of this tree 
         QuadTree topLeftTree;
@@ -22,5 +22,104 @@ namespace QuadTree
         }
 
         // methods
+
+        public void Insert(Point point)
+        {
+            if(point == null)
+            {
+                return;
+            }
+
+            if(InBoundary(point.Position) == false)
+            {
+                return;
+            }
+
+
+            if(Math.Abs(TopLeft.X - BotRight.X) <= 1)
+            {
+                if(Math.Abs(TopLeft.Y - BotRight.Y) <= 1)
+                {
+                    if(CurrentPoint == null)
+                    {
+                        CurrentPoint = point;
+                        return;
+                    }
+                }
+            }
+
+            // checking left
+            if((TopLeft.X + BotRight.X) / 2 >= point.Position.X)
+            {
+                // TopLeftTree
+                if((TopLeft.Y + BotRight.Y) / 2 >= point.Position.Y)
+                {
+                    if (topLeftTree == null)
+                    {
+                        topLeftTree = new QuadTree(
+                            new Position(TopLeft.X, TopLeft.Y),
+                            new Position((TopLeft.X + BotRight.X) / 2, (TopLeft.Y + BotRight.Y) / 2)
+                            );
+                    }
+                    topLeftTree.Insert(point);
+                }
+                else // BotLeftTree
+                {
+                    if(botLeftTree == null)
+                    {
+                        botLeftTree = new QuadTree(
+                            new Position(TopLeft.X, (TopLeft.Y + BotRight.Y) / 2),
+                            new Position((TopLeft.X + BotRight.X) / 2, BotRight.Y)
+                    );
+                    }
+                    botLeftTree.Insert(point);
+                }
+            }
+            else // checking right
+            {
+                // TopRightTree
+                if ((TopLeft.Y + BotRight.Y) / 2 >= point.Position.Y)
+                {
+                    if (topLeftTree == null)
+                    {
+                        topLeftTree = new QuadTree(
+                            new Position((TopLeft.X + BotRight.X) / 2, TopLeft.Y),
+                            new Position(BotRight.X, (TopLeft.Y + BotRight.Y) / 2)
+                            );
+                    }
+                    topRightTree.Insert(point);
+                }
+                else // BotRightTree
+                {
+                    if (botLeftTree == null)
+                    {
+                        botLeftTree = new QuadTree(
+                            new Position((TopLeft.X + BotRight.X) / 2, (TopLeft.Y + BotRight.Y) / 2),
+                            new Position(BotRight.X, BotRight.Y)
+                    );
+                    }
+                    botRightTree.Insert(point);
+                }
+            }
+            
+        } 
+
+        public bool InBoundary(Position position)
+        {
+            // checking X
+            if(position.X >= TopLeft.X && position.X <= BotRight.X)
+            {
+                // checking Y
+                if(position.Y >= TopLeft.Y && position.Y <= BotRight.Y)
+                {
+                    // position is inside boundary
+                    return true;
+                }
+            }
+
+            // position is outside boundary
+            return false;
+        }
+
     }
 }
