cecab4a7973ec421b1c58fac89fdd63c442d5b01
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2020-04-13 13:56:39 +0200

    int -> short, new points

--- PointsHistogram/QuadTree/Point.cs
+++ PointsHistogram/QuadTree/Point.cs
@@ -3,8 +3,17 @@ namespace QuadTree
 {
     public class Point
     {
-        public Point()
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public short I { get; set; }
+
+        public Point(double x, double y, double z, short i)
         {
+            X = x;
+            Y = y;
+            Z = z;
+            I = i;
         }
     }
 }
