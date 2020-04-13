caf9dec156fa774c97f39db2af5562c272768f37
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2020-04-14 00:38:08 +0200

    Tested QuadTree Count

--- PointsHistogram/QuadTree/Program.cs
+++ PointsHistogram/QuadTree/Program.cs
@@ -5,6 +5,8 @@ namespace QuadTree
 {
     class MainClass
     {
+        static int Count = 0;
+
         public static int Main(string[] args)
         {
             /*if (args.Length < 2)
@@ -14,120 +16,17 @@ namespace QuadTree
             }
             else
             {*/
-            QuadTree quadTree = new QuadTree(0, 0, 9, 9);
-            quadTree.TryToInsert(new Point(0, 0, 0, 0));
-            quadTree.TryToInsert(new Point(0, 1, 0, 0));
-            quadTree.TryToInsert(new Point(0, 2, 0, 0));
-            quadTree.TryToInsert(new Point(0, 3, 0, 0));
-            quadTree.TryToInsert(new Point(0, 4, 0, 0));
-            quadTree.TryToInsert(new Point(0, 5, 0, 0));
-            quadTree.TryToInsert(new Point(0, 6, 0, 0));
-            quadTree.TryToInsert(new Point(0, 7, 0, 0));
-            quadTree.TryToInsert(new Point(0, 8, 0, 0));
-            quadTree.TryToInsert(new Point(0, 9, 0, 0));
 
-            quadTree.TryToInsert(new Point(1, 0, 0, 0));
-            quadTree.TryToInsert(new Point(1, 1, 0, 0));
-            quadTree.TryToInsert(new Point(1, 2, 0, 0));
-            quadTree.TryToInsert(new Point(1, 3, 0, 0));
-            quadTree.TryToInsert(new Point(1, 4, 0, 0));
-            quadTree.TryToInsert(new Point(1, 5, 0, 0));
-            quadTree.TryToInsert(new Point(1, 6, 0, 0));
-            quadTree.TryToInsert(new Point(1, 7, 0, 0));
-            quadTree.TryToInsert(new Point(1, 8, 0, 0));
-            quadTree.TryToInsert(new Point(1, 9, 0, 0));
+            double minX = 394364;
+            double maxX = 394374;
+            double minY = 39150;
+            double maxY = 39160;
 
-            quadTree.TryToInsert(new Point(2, 0, 0, 0));
-            quadTree.TryToInsert(new Point(2, 1, 0, 0));
-            quadTree.TryToInsert(new Point(2, 2, 0, 0));
-            quadTree.TryToInsert(new Point(2, 3, 0, 0));
-            quadTree.TryToInsert(new Point(2, 4, 0, 0));
-            quadTree.TryToInsert(new Point(2, 5, 0, 0));
-            quadTree.TryToInsert(new Point(2, 6, 0, 0));
-            quadTree.TryToInsert(new Point(2, 7, 0, 0));
-            quadTree.TryToInsert(new Point(2, 8, 0, 0));
-            quadTree.TryToInsert(new Point(2, 9, 0, 0));
-
-            quadTree.TryToInsert(new Point(3, 0, 0, 0));
-            quadTree.TryToInsert(new Point(3, 1, 0, 0));
-            quadTree.TryToInsert(new Point(3, 2, 0, 0));
-            quadTree.TryToInsert(new Point(3, 3, 0, 0));
-            quadTree.TryToInsert(new Point(3, 4, 0, 0));
-            quadTree.TryToInsert(new Point(3, 5, 0, 0));
-            quadTree.TryToInsert(new Point(3, 6, 0, 0));
-            quadTree.TryToInsert(new Point(3, 7, 0, 0));
-            quadTree.TryToInsert(new Point(3, 8, 0, 0));
-            quadTree.TryToInsert(new Point(3, 9, 0, 0));
-
-            quadTree.TryToInsert(new Point(4, 0, 0, 0));
-            quadTree.TryToInsert(new Point(4, 1, 0, 0));
-            quadTree.TryToInsert(new Point(4, 2, 0, 0));
-            quadTree.TryToInsert(new Point(4, 3, 0, 0));
-            quadTree.TryToInsert(new Point(4, 4, 0, 0));
-            quadTree.TryToInsert(new Point(4, 5, 0, 0));
-            quadTree.TryToInsert(new Point(4, 6, 0, 0));
-            quadTree.TryToInsert(new Point(4, 7, 0, 0));
-            quadTree.TryToInsert(new Point(4, 8, 0, 0));
-            quadTree.TryToInsert(new Point(4, 9, 0, 0));
-
-            quadTree.TryToInsert(new Point(5, 0, 0, 0));
-            quadTree.TryToInsert(new Point(5, 1, 0, 0));
-            quadTree.TryToInsert(new Point(5, 2, 0, 0));
-            quadTree.TryToInsert(new Point(5, 3, 0, 0));
-            quadTree.TryToInsert(new Point(5, 4, 0, 0));
-            quadTree.TryToInsert(new Point(5, 5, 0, 0));
-            quadTree.TryToInsert(new Point(5, 6, 0, 0));
-            quadTree.TryToInsert(new Point(5, 7, 0, 0));
-            quadTree.TryToInsert(new Point(5, 8, 0, 0));
-            quadTree.TryToInsert(new Point(5, 9, 0, 0));
-
-            quadTree.TryToInsert(new Point(6, 0, 0, 0));
-            quadTree.TryToInsert(new Point(6, 1, 0, 0));
-            quadTree.TryToInsert(new Point(6, 2, 0, 0));
-            quadTree.TryToInsert(new Point(6, 3, 0, 0));
-            quadTree.TryToInsert(new Point(6, 4, 0, 0));
-            quadTree.TryToInsert(new Point(6, 5, 0, 0));
-            quadTree.TryToInsert(new Point(6, 6, 0, 0));
-            quadTree.TryToInsert(new Point(6, 7, 0, 0));
-            quadTree.TryToInsert(new Point(6, 8, 0, 0));
-            quadTree.TryToInsert(new Point(6, 9, 0, 0));
-
-            quadTree.TryToInsert(new Point(7, 0, 0, 0));
-            quadTree.TryToInsert(new Point(7, 1, 0, 0));
-            quadTree.TryToInsert(new Point(7, 2, 0, 0));
-            quadTree.TryToInsert(new Point(7, 3, 0, 0));
-            quadTree.TryToInsert(new Point(7, 4, 0, 0));
-            quadTree.TryToInsert(new Point(7, 5, 0, 0));
-            quadTree.TryToInsert(new Point(7, 6, 0, 0));
-            quadTree.TryToInsert(new Point(7, 7, 0, 0));
-            quadTree.TryToInsert(new Point(7, 8, 0, 0));
-            quadTree.TryToInsert(new Point(7, 9, 0, 0));
-
-            quadTree.TryToInsert(new Point(8, 0, 0, 0));
-            quadTree.TryToInsert(new Point(8, 1, 0, 0));
-            quadTree.TryToInsert(new Point(8, 2, 0, 0));
-            quadTree.TryToInsert(new Point(8, 3, 0, 0));
-            quadTree.TryToInsert(new Point(8, 4, 0, 0));
-            quadTree.TryToInsert(new Point(8, 5, 0, 0));
-            quadTree.TryToInsert(new Point(8, 6, 0, 0));
-            quadTree.TryToInsert(new Point(8, 7, 0, 0));
-            quadTree.TryToInsert(new Point(8, 8, 0, 0));
-            quadTree.TryToInsert(new Point(8, 9, 0, 0));
-
-            quadTree.TryToInsert(new Point(9, 0, 0, 0));
-            quadTree.TryToInsert(new Point(9, 1, 0, 0));
-            quadTree.TryToInsert(new Point(9, 2, 0, 0));
-            quadTree.TryToInsert(new Point(9, 3, 0, 0));
-            quadTree.TryToInsert(new Point(9, 4, 0, 0));
-            quadTree.TryToInsert(new Point(9, 5, 0, 0));
-            quadTree.TryToInsert(new Point(9, 6, 0, 0));
-            quadTree.TryToInsert(new Point(9, 7, 0, 0));
-            quadTree.TryToInsert(new Point(9, 8, 0, 0));
-            quadTree.TryToInsert(new Point(9, 9, 0, 0));
-            /*try
+            QuadTree quadTree = new QuadTree(minX, minY, maxX, maxY); 
+            try
             {
                 // Read file using StreamReader. Reads file line by line    
-                using (StreamReader file = new StreamReader("test.txt")) // (args[0])
+                using (StreamReader file = new StreamReader("Korte_Vegetation_all.txt")) // (args[0])
                 {
                     string ln;
 
@@ -154,10 +53,9 @@ namespace QuadTree
                 Console.WriteLine(e.Message);
             }
 
-        //}
-        */
-
             DisplayQuadTree(quadTree);
+            Console.WriteLine(Count);
+
 
             return 0;
         }
@@ -187,6 +85,7 @@ namespace QuadTree
         private static void DisplayPoint(Point point)
         {
             Console.WriteLine(point.X + " " + point.Y + ": " + point.I);
+            Count++;
         }
     }
 }
