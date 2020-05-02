96815e21e33d16fa556e077df5d5e610f003d179
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2020-05-02 15:30:50 +0200

    Load file with data

--- LinearRegression/LinearRegression/Program.cs
+++ LinearRegression/LinearRegression/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace LinearRegression
 {
@@ -6,7 +8,66 @@ namespace LinearRegression
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string fileName = "data.txt"; //single.txt // multi.txt
+            List<double> outputs = new List<double>();
+            List<List<double>> columnsWithInputs = new List<List<double>>();
+
+            // Read file using StreamReader. Reads file line by line    
+            using (StreamReader file = new StreamReader(fileName))
+            {
+                string ln;
+
+                ln = file.ReadLine();
+                int numberOfVariables = HowManyVariablesInLine(ln);
+                CreateColumns(columnsWithInputs, numberOfVariables);
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    ln = ln.Replace(".", ",");
+                    string[] tokens = ln.Split(' ');
+
+                    for(int i = 0; i < numberOfVariables; i++)
+                    {
+                        if (i + 1 == numberOfVariables) // save output
+                        {
+                            double value = double.Parse(tokens[i]);
+                            outputs.Add(value);
+                        }
+                        else // save input
+                        {
+                            double value = double.Parse(tokens[i]);
+                            columnsWithInputs[i].Add(value);
+                        }
+                    }
+
+                }
+                file.Close();
+            }
+            FunctionMatrices matrices = new FunctionMatrices(columnsWithInputs, outputs);
+            RegressionCalculator calculator = new RegressionCalculator(matrices);
+
+            Console.WriteLine(calculator.B);
+        }
+
+        private static void CreateColumns(List<List<double>> columnsWithInputs, int numberOfVariables)
+        {
+            int numberOfColumns = numberOfVariables - 1;
+            for(int i = 0; i < numberOfColumns; i++)
+            {
+                columnsWithInputs.Add(new List<double>());
+            }
+        }
+
+        private static int HowManyVariablesInLine(string ln)
+        {
+            int count = 1; // one is the result
+            foreach(char x in ln)
+            {
+                if (x == ' ')
+                    count++;
+            }
+
+            return count;
         }
     }
 }
